d4f84f2466ee02ac02a3862ae104ff615125e8ad
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-10 16:59:18 +0100

    add button to the home page to use the store function

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { useForm } from '@inertiajs/react';
+import React from 'react';
+
+export default function Home({ steam }) {
+    console.log(steam);  // This will log the "steam" value
+    const {post} = useForm({});
+
+    function submit(e) {
+        e.preventDefault();
+        post("/inputGames");
+    }
+
     return (
         <>
-            <h1>hello user</h1>
+
+
+            <form>
+                <button>Add Games
+                </button></form>
+            <h1>Game News:</h1>
+               
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\SteamController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::get('/', [SteamController::class, 'test']);
+
+Route::post('/inputGames', [SteamController::class, 'store']);
