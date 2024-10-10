a275e777f5fa3b4dd210ce1883ad67ac95c816c7
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-10 16:59:00 +0100

    add store function to call from the api to my own db

--- app/Http/Controllers/SteamController.php
+++ app/Http/Controllers/SteamController.php
@@ -2,14 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Steam;
 use Syntax\SteamApi\Facades\SteamApi;
+use Inertia\Inertia;
 
 class SteamController
 {
     public function test()
     {
-        // Get Portal 2 details
-        $apps = SteamApi::app()->appDetails(10);
-        echo $apps->first()->name;
+        // Get news
+        $steam = '';
+        // Return to Inertia 'Home' component with 'steam' as a prop
+        return Inertia::render('Home', ['steam' => $steam]);
     }
+
+    public function store()
+    {     
+        // Increase the execution time to go through the whole array   
+        ini_set('max_execution_time', 500);
+        
+        // Get all appIds from Steam
+        $steamApps = SteamApi::app()->GetAppList(50);
+
+        // Assuming the returned structure is an array of stdClass objects with 'appid' and 'name'
+        foreach ($steamApps as $game) {
+            // Access properties using the object notation
+            if (isset($game->appid, $game->name)) {
+                // Use Create correctly
+                Steam::create([
+                    'appId' => $game->appid, 
+                    'name' => $game->name    
+                ]);
+            }
+        }
+
+        // Resets the max execution time back to 60sec  
+        ini_set('max_execution_time', 60);
+    }
+
+
 }
