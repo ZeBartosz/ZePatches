dd7087ac3b2071ad2ab6f95ad0864040332b60a9
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-11 23:57:20 +0100

    remove convertToHTML and updated the css

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,59 +1,26 @@
 import React, { useState } from "react";
+import { convertToHTML } from '../app.js'
 
 export default function Show({ minorPatches, majorPatches, gameName }) {
     
     // State to track active tab
     const [activeTab, setActiveTab] = useState("Table1"); 
 
-    const convertToHTML = (text) => {
-        // Replace custom syntax with HTML tags
-        const formattedText = text
-            .replace(/\[list\]/g, "<ul>")
-            .replace(/\[\/list\]/g, "</ul>")
-            .replace(/\[\*\]/g, "<li>")
-            .replace(/\[\/\*\]/g, "</li>")
-            .replace(/\[h3\]/g, "<h3>")
-            .replace(/\[\/h3\]/g, "</h3>")
-            .replace(/\[h2\]/g, "<h2>")
-            .replace(/\[\/h2\]/g, "</h2>")
-            .replace(/\[b\]/g, "<strong>")
-            .replace(/\[\/b\]/g, "</strong>")
-            .replace(/\[i\]/g, "<em>")
-            .replace(/\[\/i\]/g, "</em>")
-            .replace(
-                /\[url=(.+?)\](.+?)\[\/url\]/g,
-                '<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>'
-            )
-            .replace(
-                /\[img\](https?:\/\/.+?)\[\/img\]/g,
-                '<div class="flex justify-center p-2"><img width="560" height="315" src="$1" alt="Image" /></div>'
-            )
-            .replace(
-                /\[img\]{STEAM_CLAN_IMAGE}\/(.+?)\[\/img\]/g,
-                '<div class="flex justify-center p-2"><img width="560" height="315" src="https://clan.akamai.steamstatic.com/images/$1" alt="Image" /></div>'
-            )
-            .replace(
-                /\[previewyoutube=(.+?);full\]\[\/previewyoutube\]/g,
-                '<div class="flex justify-center p-2"><iframe width="560" height="315" src="https://www.youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
-            );
-
-        return formattedText;
-    };
-
     return (
         <>
-            <div className="tab flex justify-center text-white rounded-md border-2 bg-gray-900 bg-opacity-75 my-3 mx-10">
+            <h1 className="pt-2 my-0 text-[#c7d5e0]">{gameName}</h1>
+            <div className="tab flex justify-evenly bg-[#2a475e] border-2 text-[#c7d5e0] border-[#66c0f4] rounded-md m-2 p-1">
                 <button
-                    className={`tablinks ${
-                        activeTab === "Table1" ? "active" : ""
+                    className={`m-1 p-2 rounded-md tablinks ${
+                        activeTab === "Table1" ? "active bg-gray-900" : ""
                     }`}
                     onClick={() => setActiveTab("Table1")}
                 >
                     Major
                 </button>
                 <button
-                    className={`tablinks ${
-                        activeTab === "Table2" ? "active" : ""
+                    className={`m-1 p-2 rounded-md tablinks ${
+                        activeTab === "Table2" ? "active bg-gray-800" : ""
                     }`}
                     onClick={() => setActiveTab("Table2")}
                 >
@@ -61,16 +28,14 @@ export default function Show({ minorPatches, majorPatches, gameName }) {
                 </button>
             </div>
 
-            <h1 className="py-3 mt-0 text-[#c7d5e0]">{gameName}</h1>
-
             {/* Major Patches Table */}
             <div
                 id="Table1"
                 className={`table-content ${
                     activeTab === "Table1" ? "active" : "hidden"
-                } bg-gray-700 bg-opacity-75 border-2 rounded-lg text-white`}
+                } bg-gray-900 bg-opacity-75 border-2 border-[#66c0f4] rounded-lg text-white`}
             >
-                <h1 className="py-3 mt-0 text-[#c7d5e0]">Major</h1>
+                <h1 className="py-3  my-0 text-[#c7d5e0] border-0">Major</h1>
                 {majorPatches
                     ? majorPatches.events.map((patch) => (
                           <div
@@ -100,9 +65,9 @@ export default function Show({ minorPatches, majorPatches, gameName }) {
                 id="Table2"
                 className={`table-content ${
                     activeTab === "Table2" ? "active" : "hidden"
-                } bg-gray-900 bg-opacity-75 border-2 rounded-lg text-white`}
+                } bg-gray-800 bg-opacity-75 border-[#66c0f4] border-2 rounded-lg text-white`}
             >
-                <h1 className="py-3 mt-0 text-[#c7d5e0]">Minor</h1>
+                <h1 className="py-3 my-0 text-[#c7d5e0] border-0">Minor</h1>
                 {minorPatches
                     ? minorPatches.events.map((patch) => (
                           <div
