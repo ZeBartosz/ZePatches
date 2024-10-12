4d9970b742489543f09e620bfc09046e1ac51da0
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-12 11:57:01 +0100

    add comments to infinite scroll

--- resources/js/Layouts/Patches.jsx
+++ resources/js/Layouts/Patches.jsx
@@ -6,21 +6,30 @@ function Patches({ patch, activeTab, tableId, title }) {
     const [currentPatches, setCurrentPatches] = useState([]);
     const [hasMore, setHasMore] = useState(true);
 
-    // Load initial patches on component mount
+    // Hook to manage side effects and lifecycle events
     useEffect(() => {
-        // Load first 10 patches
-        setCurrentPatches(patch.events.slice(0, 3)); 
-        // Set hasMore if there are more than 10
-        setHasMore(patch.events.length > 3); 
+        // Load the first 3 patches from the provided patch.events array
+        setCurrentPatches(patch.events.slice(0, 3));
+
+        // If there are more than 3 patches, set the hasMore state to true
+        setHasMore(patch.events.length > 3);
+
     }, [patch.events]);
 
+    // Function to load more patches when user scrolls
     const fetchMorePatches = () => {
-        const nextBatch = patch.events.slice(currentPatches.length, currentPatches.length + 3);
+        // Calculate the next batch of patches to be loaded.
+        const nextBatch = patch.events.slice(
+            currentPatches.length,
+            currentPatches.length + 3
+        );
+
+        // Update the currentPatches state by adding the newly fetched patches to the previous ones
         setCurrentPatches((prevPatches) => [...prevPatches, ...nextBatch]);
 
-        // No more patches to load
+        // Check if all patches have been loaded
         if (currentPatches.length + nextBatch.length >= patch.events.length) {
-            setHasMore(false); 
+            setHasMore(false);
         }
     };
 
@@ -41,7 +50,11 @@ function Patches({ patch, activeTab, tableId, title }) {
                         next={fetchMorePatches}
                         hasMore={hasMore}
                         loader={<h4>Loading more patches...</h4>}
-                        endMessage={<p className="text-center">No more {title} patches to load</p>}
+                        endMessage={
+                            <p className="text-center">
+                                No more {title} patches to load
+                            </p>
+                        }
                     >
                         {currentPatches.map((patch) => (
                             <div
