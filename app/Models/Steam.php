8f6f6bff0d113f18e838bb3edbe9ea7c17cd4fbf
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-16 12:55:39 +0100

    add laravel scout to search for games

--- app/Models/Steam.php
+++ app/Models/Steam.php
@@ -6,12 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Scout\Searchable;
 use Syntax\SteamApi\Facades\SteamApi;
 
 class Steam extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $fillable = [
         'name',
@@ -23,9 +24,25 @@ class Steam extends Authenticatable
         'moreDetails',
     ];
 
+    public function toSearchableArray(): array
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+
     public static function findGameByName(String $game)
     {
-        return self::where('name', 'LIKE', "%$game%")->orderBy('type')->get();
+        return self::search($game)->query(function ($query) {
+            $query->orderByRaw("
+            CASE 
+                WHEN type = 'game' THEN 0
+                WHEN type = 'dlc' THEN 1
+                WHEN type = 'Unknown Type' THEN 3
+                ELSE 2
+            END
+                ")->orderByRaw('LENGTH(name) ASC');
+        });
     }
 
     public static function findGameByAppId($appId)
