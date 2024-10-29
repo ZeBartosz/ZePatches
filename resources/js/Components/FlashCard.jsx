9b8334afac93aa97aab45f3eb63fe6b4229f152f
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-29 14:38:56 +0000

    add transition to div

--- resources/js/Components/FlashCard.jsx
+++ resources/js/Components/FlashCard.jsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
 
 function FlashCard({ message }) {
-    const [visibility, setvisibility] = useState("invisible");
+    const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
-        setvisibility("visible");
-        setTimeout(() => {
-            setvisibility("invisible");
-        }, 5000);
+        if (message) {
+            setIsVisible(true);
+            const timer = setTimeout(() => {
+                setIsVisible(false);
+            }, 5000);
+            return () => clearTimeout(timer);
+        }
     }, [message]);
 
     return (
         <>
-            {message ? (
+            {message && (
                 <div
-                    className={`fixed z-50 bottom-24 right-[-5px] border-2 border-green-500 rounded-md bg-[#2a475e] max-w-56 ${visibility}`}
+                    className={`fixed z-50 bottom-24 right-[-5px] border-2 border-green-500 rounded-md bg-[#2a475e] max-w-56 transition-all duration-500 overflow-x-hidden ${
+                        isVisible ? "opacity-100" : "opacity-0 translate-x-52 "
+                    }`}
                 >
                     <p className="p-3 text-green-500 ">{message}</p>
                 </div>
-            ) : (
-                ""
             )}
         </>
     );
