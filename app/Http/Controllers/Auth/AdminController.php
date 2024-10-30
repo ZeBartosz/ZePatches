d43b17944cbe91f0d6be320e1909000d90953241
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-30 15:27:39 +0000

    add tables for user and topFavorite

--- app/Http/Controllers/Auth/AdminController.php
+++ app/Http/Controllers/Auth/AdminController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAdminRequest;
 use App\Http\Requests\UpdateAdminRequest;
 use App\Jobs\ProcessSteam;
+use App\Models\Favorite;
 use App\Models\Steam;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,8 +19,14 @@ class AdminController extends Controller
     public function show()
     {
         $totalGameCount = Steam::count();
+        $topFavorites = Steam::withCount(['favorites'])
+            ->whereHas('favorites')
+            ->orderByDesc('favorites_count')
+            ->limit(10)->get();
+        $users = User::withCount(['favorites'])->get();
 
-        return inertia('Auth/AdminDashboard', ['gameCount' => $totalGameCount]);
+
+        return inertia('Auth/AdminDashboard', ['gameCount' => $totalGameCount, 'users' => $users, 'topFavorite' => $topFavorites]);
     }
 
     public function FetchGamesFromAPI()
--- resources/js/Pages/Auth/AdminDashboard.jsx
+++ resources/js/Pages/Auth/AdminDashboard.jsx
@@ -1,12 +1,19 @@
 import { useForm } from "@inertiajs/react";
 
-export default function AdminDashboard({ authUser, gameCount }) {
+export default function AdminDashboard({
+    authUser,
+    gameCount,
+    users,
+    topFavorite,
+}) {
     const { post, processing } = useForm({});
 
     const FetchNewGames = (e) => {
         e.preventDefault();
         post("/fetchGames");
     };
+
+    console.log(topFavorite);
 
     return (
         <>
@@ -15,6 +22,76 @@ export default function AdminDashboard({ authUser, gameCount }) {
             <form onSubmit={FetchNewGames}>
                 <button className="text-white">Fetch New Games</button>
             </form>
+
+            <div>
+                <table className="table-fixed border-collapse border border-slate-500">
+                    <tr>
+                        <th className="border border-slate-600">Avatar</th>
+                        <th className="border border-slate-600">Username</th>
+                        <th className="border border-slate-600">
+                            Favorite_Count
+                        </th>
+                        <th className="border border-slate-600">Is_Admin</th>
+                        <th className="border border-slate-600">Other</th>
+                    </tr>
+
+                    {users.map((user) => (
+                        <tr key={user.steam_id}>
+                            <td className="border border-slate-700">
+                                <img src={user.avatar} alt="" />
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{user.nickname}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{user.favorites_count}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{user.is_admin}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>promote</p>
+                            </td>
+                        </tr>
+                    ))}
+                </table>
+
+                <table className="table-fixed border-collapse mt-1">
+                    <tr>
+                        <th className="border border-slate-600">Name</th>
+                        <th className="border border-slate-600">AppId</th>
+                        <th className="border border-slate-600">Developer</th>
+                        <th className="border border-slate-600">releaseDate</th>
+                        <th className="border border-slate-600">Type</th>
+                        <th className="border border-slate-600">
+                            Favorite_count
+                        </th>
+                    </tr>
+
+                    {topFavorite.map((favorite) => (
+                        <tr key={favorite.appId} className={`bg-gradient-to-l`}>
+                            <td className="border border-slate-700">
+                                <p>{favorite.name}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{favorite.appId}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{favorite.developer}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{favorite.releaseDate}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{favorite.type}</p>
+                            </td>
+                            <td className="border border-slate-700">
+                                <p>{favorite.favorites_count}</p>
+                            </td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
         </>
     );
 }
