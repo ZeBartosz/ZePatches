efe0f26cef1ed2119bc48f7d5efd7a19d9197d01
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-10-31 19:52:34 +0000

    add model for patches

---
+++ app/Http/Controllers/PatchesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patches;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePatchesRequest;
+use App\Http\Requests\UpdatePatchesRequest;
+
+class PatchesController extends Controller
+{
+    //
+}
---
+++ database/migrations/2024_10_31_194911_create_patches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Steam;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Steam::class)->constrained();
+            $table->string('latestPatchDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patches');
+    }
+};
