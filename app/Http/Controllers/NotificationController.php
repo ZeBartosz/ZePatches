335469579615f0eadecf5e06ab78d98c91828dab
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-11-05 12:29:09 +0000

    add notification model

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreNotificationRequest;
+use App\Http\Requests\UpdateNotificationRequest;
+
+class NotificationController extends Controller
+{
+    //  
+}
