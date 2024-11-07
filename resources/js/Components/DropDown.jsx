c9aed4d779679a7b92a7be3ca145b1c8e0f36416
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-11-07 10:19:16 +0000

    add notficiation tab on button press

--- resources/js/Components/DropDown.jsx
+++ resources/js/Components/DropDown.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function DropDown({}) {
     const [active, setActive] = useState(false);
+    const [pressed, setPressed] = useState(false);
     const { post, logoutprocessing } = useForm({});
     const { authUser } = usePage().props;
     const { notifications } = usePage().props;
@@ -60,14 +61,16 @@ function DropDown({}) {
                         <div className="relative">
                             {notifications.length > 0 ? (
                                 <div className="absolute -top-2 right-4">
-                                    <p className="py-0 px-2 m-0 border rounded-full border-[#2a475e] bg-red-500">
+                                    <p className="py-0 px-2 m-0 border rounded-full border-[#66c0f4] bg-red-500">
                                         {notifications.length}
                                     </p>
                                 </div>
                             ) : (
                                 ""
                             )}
-                            <button>Notifications</button>
+                            <button onClick={() => setPressed(true)}>
+                                Notifications
+                            </button>
                             <form onSubmit={logout}>
                                 <button className="" type="submit">
                                     {logoutprocessing
@@ -88,6 +91,64 @@ function DropDown({}) {
                     </button>
                 </div>
             )}
+
+            <div
+                className={`fixed z-50 w-4/5 top-5 bg-black text-white bg-opacity-100 left-1/2 transform -translate-x-1/2 rounded-md shadow-[0_35px_60px_-15px_rgba(0,0,0,1)] ${
+                    pressed ? "active" : "hidden"
+                }`}
+            >
+                <h1 className="m-1 p-1 border-[#66c0f4]">
+                    You currently have {notifications.length} notifications
+                </h1>
+
+                {notifications.length > 0 ? (
+                    <>
+                        {notifications.map((notification) => (
+                            <div
+                                key={notification.id}
+                                className={`p-1 flex justify-between m-3 border-b  ${
+                                    pressed ? "active" : "hidden"
+                                }`}
+                            >
+                                <h2>{notification.game}</h2>
+                                <p className="pt-4 pr-0 m-0">
+                                    AppId {notification.appId}
+                                </p>
+                                {notification.eventName ? (
+                                    <div className="">
+                                        <p className="pt-4 px-0 pb-0 m-0">
+                                            {notification.eventName}
+                                        </p>
+                                        <p className="p-0 m-0">
+                                            {notification.eventPatchesDate}
+                                        </p>
+                                    </div>
+                                ) : (
+                                    <div className="">
+                                        <p className="pt-4 px-0 pb-0 m-0">
+                                            {notification.patchName}
+                                        </p>
+                                        <p className="p-0 m-0">
+                                            {notification.patchNotesDate}
+                                        </p>
+                                    </div>
+                                )}
+                                <Link
+                                    className="pt-4 m-0"
+                                    href={`game/${notification.appId}`}
+                                >
+                                    View Update
+                                </Link>
+                            </div>
+                        ))}
+                        <div></div>
+                    </>
+                ) : (
+                    <div>
+                        <p>No new notifications</p>
+                    </div>
+                )}
+            </div>
         </>
     );
 }
