a5fea76e5edc27f83f7751e7e09a09ab267f7ee3
Bartosz Stachowiak <stachowiak725@gmail.com>
2024-11-27 13:16:59 +0000

    update to use toast

--- resources/js/Components/FlashCard.jsx
+++ resources/js/Components/FlashCard.jsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, React } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function FlashCard({ message }) {
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
         if (message) {
-            setIsVisible(true);
-            const timer = setTimeout(() => {
-                setIsVisible(false);
-            }, 5000);
-            return () => clearTimeout(timer);
+            toast(message);
         }
     }, [message]);
 
-    return (
-        <>
-            {message && (
-                <div
-                    className={`fixed z-50 bottom-24 right-[-5px] border-2 border-green-500 rounded-md bg-[#2a475e] max-w-56 transition-all duration-500 overflow-x-hidden ${
-                        isVisible ? "opacity-100" : "opacity-0 translate-x-52 "
-                    }`}
-                >
-                    <p className="p-3 text-green-500 ">{message}</p>
-                </div>
-            )}
-        </>
-    );
+    return <ToastContainer />;
 }
 
 export default FlashCard;
